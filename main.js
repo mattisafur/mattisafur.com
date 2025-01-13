c8f9cf607fdda03c59837d4c72c94f7f00fc6ac7
mattisafur <46404761+mattisafur@users.noreply.github.com>
2025-01-14 00:03:05 +0200

    wraped js in anon function

--- main.js
+++ main.js
@@ -1,76 +1,80 @@
 (function () {
   // add nav bar
-  document.querySelector("body").insertAdjacentHTML(
-    "afterbegin",
-    `
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">[Home]</a>
-                </li>
-                <li>
-                    <a href="/projects">[Projects]</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    `
-  );
+  (function () {
+    document.querySelector("body").insertAdjacentHTML(
+      "afterbegin",
+      `
+      <header>
+          <nav>
+              <ul>
+                  <li>
+                      <a href="/">[Home]</a>
+                  </li>
+                  <li>
+                      <a href="/projects">[Projects]</a>
+                  </li>
+              </ul>
+          </nav>
+      </header>
+      `
+    );
+  })();
 
   // format code blocks
-  document.querySelectorAll("pre:has(code)").forEach((preBlock) => {
-    // trim whitespace chars
-    preBlock.innerHTML = preBlock.innerHTML.trim();
+  (function () {
+    document.querySelectorAll("pre:has(code)").forEach((preBlock) => {
+      // trim whitespace chars
+      preBlock.innerHTML = preBlock.innerHTML.trim();
 
-    preBlock.querySelectorAll("code").forEach((codeBlock) => {
-      // format code
-      (function () {
-        // trim empty lines at start and end
-        const trimmedCode = codeBlock.textContent.replaceAll(
-          /(^\s*\n)|(\n\s*$)/g,
-          ""
-        );
-
-        // remove excess indentation
-        let lines = trimmedCode.split("\n");
-        let minWhitespaces = Number.MAX_VALUE;
-        lines.forEach((line) => {
-          if (!line.trim()) {
-            return;
-          }
-
-          minWhitespaces = Math.min(
-            minWhitespaces,
-            RegExp(/^\s*/).exec(line)[0].length || 0
+      preBlock.querySelectorAll("code").forEach((codeBlock) => {
+        // format code
+        (function () {
+          // trim empty lines at start and end
+          const trimmedCode = codeBlock.textContent.replaceAll(
+            /(^\s*\n)|(\n\s*$)/g,
+            ""
           );
-        });
-        lines = lines.map((line) => line.slice(minWhitespaces));
 
-        // surround each line in <span> element
-        codeBlock.innerHTML = lines
-          .map((line) => `<span>${line}</span>`)
-          .join("");
-      })();
+          // remove excess indentation
+          let lines = trimmedCode.split("\n");
+          let minWhitespaces = Number.MAX_VALUE;
+          lines.forEach((line) => {
+            if (!line.trim()) {
+              return;
+            }
 
-      // format line numbering
-      (function () {
-        const lines = codeBlock.querySelectorAll("span");
-        const lineDigits = lines.length.toString().length;
-        const uniqueClass = `code-${crypto.randomUUID()}`;
+            minWhitespaces = Math.min(
+              minWhitespaces,
+              RegExp(/^\s*/).exec(line)[0].length || 0
+            );
+          });
+          lines = lines.map((line) => line.slice(minWhitespaces));
 
-        codeBlock.classList.add(uniqueClass);
+          // surround each line in <span> element
+          codeBlock.innerHTML = lines
+            .map((line) => `<span>${line}</span>`)
+            .join("");
+        })();
 
-        const style = document.createElement("style");
-        document.head.appendChild(style);
-        style.sheet.insertRule(
-          `
+        // format line numbering
+        (function () {
+          const lines = codeBlock.querySelectorAll("span");
+          const lineDigits = lines.length.toString().length;
+          const uniqueClass = `code-${crypto.randomUUID()}`;
+
+          codeBlock.classList.add(uniqueClass);
+
+          const style = document.createElement("style");
+          document.head.appendChild(style);
+          style.sheet.insertRule(
+            `
           code.${uniqueClass} span::before {
             width: ${lineDigits}ch;
           }
           `
-        );
-      })();
+          );
+        })();
+      });
     });
-  });
+  })();
 })();
